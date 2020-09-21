52bb9514688ad0825ab2cd920fe84724c0ad286c
dongjuny <dong64553@naver.com>
2020-09-22 01:42:18 +0900

    - sequelize lib 추가 - boards 모델 정의 - gitignore에 config 폴더 추가

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-
 const routes = require('./src/routes/index');
+const sequelize = require('./models').sequelize;
 
 app.use(routes);
 
 app.listen(3000, function() {
   console.log('port 3000')
 });
+
+
+sequelize.sync(); //express 서버 실행시 스키마를 DB에 적용
